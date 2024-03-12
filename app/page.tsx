b341cefb8d376c5097d607a9a428213851c215b4
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-03-12 16:31:45 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LandingInfo from './components/LandingInfo';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-6">
       <LandingInfo />
     </main>
   );
