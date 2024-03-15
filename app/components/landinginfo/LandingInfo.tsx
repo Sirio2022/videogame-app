db689aee7dc92368a5d230cf1cb4b591a978c4f9
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2024-03-14 23:42:21 -0500

    Update LandingInfo.tsx

--- app/components/landinginfo/LandingInfo.tsx
+++ app/components/landinginfo/LandingInfo.tsx
@@ -1,7 +1,7 @@
 export default function LandingInfo() {
   return (
     <div>
-      <main className="flex flex-col items-center justify-between py-6">
+      <div className="flex flex-col items-center justify-between py-6">
         <h1 className="text-3xl font-bold text-white ">
           Welcome to the Video Game Database!
         </h1>
@@ -9,7 +9,7 @@ export default function LandingInfo() {
           This is a database of video games. You can search for games by name,
           genre, platform, and more!
         </p>
-      </main>
+      </div>
     </div>
   );
 }
